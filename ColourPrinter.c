92b79aa03d894ea0297ae5963da88a5a31f4816b
Vidur Gupta <vidurgupta01@gmail.com>
2015-05-30 17:55:14 -0700

    Added preprocessor directive to check if END_COLOUR macro exists (so that Windows compilation gives no error)

--- ColourPrinter.c
+++ ColourPrinter.c
@@ -9,7 +9,10 @@
 #include "ColourPrinter.h"
 
 void colourPrintMac(char *string, char *colour) {
+    // This preprocessor directive checks if the macro END_COLOUR is defined. When compiled with Windows, this macro is useless, and therefore we do not need to define it for Windows. We are simply ensuring that there is no exceptions when compiling on the Windows platform.
+#ifdef END_COLOUR
     printf("\033[%sm%s%s", colour, string, END_COLOUR);
+#endif
 }
 
 void colourPrintWin(char *string, char *colour) {
